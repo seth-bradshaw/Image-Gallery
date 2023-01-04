59a8c1494561b33bb31d70e56e5ea202b26d15d8
seth-bradshaw <seth.bradshaw27@gmail.com>
2023-01-04 16:19:30 -0700

    adding login endpoint

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,26 @@
-const login = async (req, res) => {
+const { client, runClientWith } = require('../mongo/client');
+const { compare } = require('./encryptionHelpers');
 
-    res.send(res.user + 'logged in')
+const login = async (req, res, next) => {
+    const { body } = req;
+    const authenticateLoginHandler = async () => {
+        const db = client.db('gallery');
+        const coll = db.collection("user");
+        const user = await coll.findOne({ username: body.username })
+
+        if (!user) {
+            res.status(401).send('Unable to locate user with given username.')
+        }
+
+        if (compare(body.password, user.password)) {
+            console.log('user', { user, body })
+            next(); // * move on to auth token middleware
+        } else {
+            console.log('incorrect password')
+            res.status(401).send('Invalid password.')
+        }
+    }
+    await runClientWith(authenticateLoginHandler);
 }
 
-// * stretch - update internal details after image transformation
-const logout = async (req, res) => {
-    res.send('logged out')
-}
-
-module.exports = { login, logout }
+module.exports = { login }
