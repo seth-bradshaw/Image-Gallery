da69d70209252464fb2f5c8156263a98e01cb566
seth-bradshaw <seth.bradshaw27@gmail.com>
2023-01-04 16:19:41 -0700

    adding create user endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const { login } = require('../controllers/auth');
 const { createUser, editUser, deleteUser } = require('../controllers/users');
+const generateAuthToken = require('../middleware/generateAuthToken');
 const router = express.Router();
 
 // TODO update these to correct CRUD method
-router.get('/create', createUser);
+router.post('/create', createUser, generateAuthToken);
 // * login user after successful creation
-router.get('/create', login);
 
-router.get('/edit', editUser);
-router.get('/delete', deleteUser)
+router.put('/edit', editUser);
+router.delete('/delete', deleteUser)
 
 module.exports = router;
