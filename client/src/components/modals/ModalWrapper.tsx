697e03e00ba4675d982b3396a1f3b8c30773f3a0
seth-bradshaw <seth.bradshaw27@gmail.com>
2023-01-07 11:26:28 -0700

    created modal wrapper

--- client/src/components/modals/ModalWrapper.tsx
+++ client/src/components/modals/ModalWrapper.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
 
-type Props = {}
+interface Props {
+  title: string;
+  handleClose: () => void;
+  dismissable?: boolean;
+}
 
-export default function ModalWrapper({}: Props) {
+export default function ModalWrapper({ children, title, handleClose }: PropsWithChildren<Props>) {
   return (
-    <div>ModalWrapper</div>
+    <div className="w-full h-screen z-40 bg-slate-300/25 block top-0 fixed shadow">
+      <div className="h-full w-full flex items-center justify-center">
+        <div className="w-full h-screen sm:w-96 sm:h-96 md:h-[480px] md:w-[480px] bg-white flex flex-col gap-2 rounded">
+            <div className="w-full shadow rounded-t h-10 bg-white text-gray-700 font-bold flex items-center text-center justify-center">
+              <h3 className="text-xl w-full ml-6">{title}</h3>
+              <button className="w-6" onClick={handleClose}>X</button>
+            </div>
+            {children}
+        </div>
+      </div>
+    </div>
   )
 }
